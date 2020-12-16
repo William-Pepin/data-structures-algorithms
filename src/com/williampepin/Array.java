8e2b36636e3bf76088db0078890fb4a561d06c3c
William Pepin <william@Williams-Macbook.local>
2020-12-16 15:58:22 -0500

    added array method, added todos in array class

--- src/com/williampepin/Array.java
+++ src/com/williampepin/Array.java
@@ -19,7 +19,7 @@ public class Array {
         items[count++] = item;
     }
 
-    public void removeAt(int index) throws IllegalArgumentException {
+    public void remove(int index) throws IllegalArgumentException {
         if(index < 0 || index >= count)
             throw new IllegalArgumentException();
 
@@ -37,9 +37,44 @@ public class Array {
         return -1;
     }
 
+    public int lastIndexOf(int item){
+        for (int i = count -1; i > -1; i++){
+            if(items[i] == item){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int size(){
+        return count;
+    }
+
+    public boolean contains(int item){
+        for (int i = 0; i < count; i++) {
+            if(items[i] == item)
+                return true;
+        }
+        return false;
+    }
+
+    public int[] toArray(){
+        int[] newItems = new int[count];
+        for (int i = 0; i < count; i++) {
+            newItems[i] = items[i];
+        }
+        return newItems;
+    }
+
     public void print(){
         for (int i = 0; i < count; i++) {
             System.out.println(items[i]);
         }
     }
+    // TODO Rendre le type dynamique
+    // TODO max() return the largest number
+    // TODO intersect() compares two array and return the number
+    // that have the same value in each of them
+    // TODO reverse() reverses the array
+    // TODO insertAt() insert at specified index
 }
--- src/com/williampepin/Main.java
+++ src/com/williampepin/Main.java
@@ -1,14 +1,16 @@
 package com.williampepin;
 
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        Array numbers = new Array(3);
-
-        numbers.insert(10);
-        numbers.insert(20);
-        numbers.print();
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(30);
+        list.add(20);
+        list.add(10);
+        System.out.println(list);
 
     }
 }
