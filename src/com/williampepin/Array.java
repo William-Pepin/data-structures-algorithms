f0b570973ec06c3a4ff66ca9bd9201b26d8b21a7
William Pepin <william@Williams-Macbook.local>
2020-12-15 22:05:25 -0500

    Array

--- src/com/williampepin/Array.java
+++ src/com/williampepin/Array.java
@@ -9,7 +9,32 @@ public class Array {
     }
 
     public void insert(int item){
+        if(items.length == count){
+            int[] newItems = new int[count*2];
+            for (int i = 0; i < count; i++) {
+                newItems[i] = items[i];
+            }
+            items = newItems;
+        }
         items[count++] = item;
+    }
+
+    public void removeAt(int index) throws IllegalArgumentException {
+        if(index < 0 || index >= count)
+            throw new IllegalArgumentException();
+
+        for (int i = index; i < count; i++) {
+            items[i] = items[i+1];
+            count--;
+        }
+    }
+
+    public int indexOf(int item) {
+        for (int i = 0; i < count; i++) {
+            if (items[i] == item)
+                return i;
+        }
+        return -1;
     }
 
     public void print(){
